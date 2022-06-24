80a0ac5c591ce755d2205640313a47b1da77ce87
PillweinJoaquin <100744806+PillweinJoaquin@users.noreply.github.com>
2022-06-24 01:51:03 -0300

    cambios

---
+++ assets/js/inicio.js
@@ -0,0 +1,22 @@
+let saludo = "Hola Clientes😊" ;
+console.log(saludo) ;
+
+
+
+function alHcerClick(){
+    document.getElementById('suscripcion').style.display = alert("las suscripciones se habilitaran pronto")
+    let confirmar = confirm("¿Desea dejar una consulta online?");
+    if(confirmar == true) {
+        window.location.replace("contacto.html")
+    }
+}
+
+
+
+
+
+
+
+    
+
+ 
